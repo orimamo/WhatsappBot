b6e0e40d038582f4e290bce8d6bcc20e352f58ee
ori mamo <mori8056@gmail.com>
2022-05-24 22:11:57 +0300

    update

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,13 +1,19 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.w3c.dom.Document;
 import org.w3c.dom.css.RGBColor;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.List;
+import java.util.Set;
 
 public class Main extends JFrame {
 
@@ -37,23 +43,28 @@ public class Main extends JFrame {
         jButton.setBounds(0,0,100,50);
 
         jButton.addActionListener((event)->{
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\mori8\\Downloads\\chromedriver_win32\\chromedriver.exe");
-            ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.addArguments("C:\\Users\\mori8\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1");
-            ChromeDriver driver=new ChromeDriver();
-            driver.get("https://web.whatsapp.com/");
-            driver.manage().window().maximize();
-            if (driver.findElement(By.className("landing-wrapper")).isDisplayed()){
-                System.out.println("`good`");
-            }
-
-
-
-
+            actionPerformed(event);
         });
 
 
 
+    }
+    private static void actionPerformed(ActionEvent event) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mori8\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("C:\\Users\\mori8\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://web.whatsapp.com/");
+        driver.manage().window().maximize();
+        boolean isConnected = false;
+        while (!isConnected) {
+            List<WebElement> sss = driver.findElements(By.id("side"));
+            if (sss.isEmpty()) {
+            } else {
+                System.out.println("you are in");
+                isConnected = true;
+            }
+        }
     }
     public void run(String s1,String s2){
         new Thread(()->{
